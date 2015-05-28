0faacd98135ff73eeb60839fd6a38adcd1ae62c7
AdamWooten <amwooten@comcast.net>
2015-05-28 14:08:42 -0700

    Created Player and Ship Class

--- app/src/main/java/com/example/mvhwhinnery/spacetrucker/Main.java
+++ app/src/main/java/com/example/mvhwhinnery/spacetrucker/Main.java
@@ -3,5 +3,12 @@ package com.example.mvhwhinnery.spacetrucker;
 /**
  * Created by mv.awooten on 5/26/2015.
  */
-public class Main {
+public class Main
+{
+
+
+    public void makeNew()
+    {
+
+    }
 }
--- app/src/main/java/com/example/mvhwhinnery/spacetrucker/Ship.java
+++ app/src/main/java/com/example/mvhwhinnery/spacetrucker/Ship.java
@@ -7,4 +7,36 @@ public class Ship
 {
     private String sName = "";
     private int cargoSize,defence,fuel;
+
+    public void setCargoSize(int cargoSize) {
+        this.cargoSize = cargoSize;
+    }
+
+    public int getCargoSize() {
+        return cargoSize;
+    }
+
+    public void setDefence(int defence) {
+        this.defence = defence;
+    }
+
+    public int getDefence() {
+        return defence;
+    }
+
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+
+    public String getsName() {
+        return sName;
+    }
 }
